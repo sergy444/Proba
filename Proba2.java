c6829fa0075489baf462c7ab634276792c1b6211
Sergy <sergy4444@gmail.com>
2014-09-18 08:50:51 +0200

    El código corregido

---
+++ Proba2.java
@@ -0,0 +1,17 @@
+https://github.com/sergy444/Proba
+
+import java.util.Scanner;
+public class Proba2 {
+public static void main (String args []){
+Scanner teclado = new Scanner(System.in);
+int numero;
+System.out.println(“Mete un número del 1 al 10”);
+Numero = teclado.nextInt();
+If (numero == 4){
+System.out.println(“Has acertado”);
+}
+else {
+System.out.println(“Fallaste”);
+}
+}
+}
