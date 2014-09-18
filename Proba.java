e7a36842321089fd1079f2d7705f1657bcf18d82
Sergy <sergy4444@gmail.com>
2014-09-18 08:28:52 +0200

    un código java

---
+++ Proba.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Prueba2 {
+public static void main (String args []){
+Scanner teclado = new Scanner(System.in);
+int numero;
+System.out.println(“Mete un número del 1 al 10”);
+Numero = teclado.nextInt();
+If (numero == 4){
+System.out.println(“Has acertado”);
+}
+else {
+System.out.println(“Fallaste”);
+}
+}
+}
